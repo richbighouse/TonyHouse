bfe6a2d88d03e12cff2bb74d8029943cda36a9ed
Richard Grand'Maison <Richard Grand'Maison>
2016-11-28 19:41:52 -0500

    Hello Tony

---
+++ src/HelloNub.java
@@ -0,0 +1,21 @@
+
+public class HelloNub {
+
+	public static void main(String[] args) {
+		
+		if(true){
+			if(!false){
+				int x = 1;
+				int y = 2;
+				if(x+y == y+x){
+					System.out.println("ouvre git bash");
+					System.out.println("cd dans le folder qui contient tes projets");
+					System.out.println("run ca : git clone https://github.com/richbighouse/TonyHouse.git TonyHouse");
+					System.out.println("GG!");
+				}
+			}
+		}
+
+	}
+
+}
