c6ecad640dfdb4dd2bbc1a3b10469139dffc017d
Gmazinn <antoinegmaison@gmail.com>
2016-11-28 20:16:55 -0500

    Signed-off-by: Gmazinn <antoinegmaison@gmail.com>

--- src/HelloNub.java
+++ src/HelloNub.java
@@ -2,9 +2,9 @@
 public class HelloNub {
 
 	public static void main(String[] args) {
-		
-		if(true){
-			if(!false){
+		boolean xyz = true;
+		if(xyz = true){
+			if(xyz != false){
 				int x = 1;
 				int y = 2;
 				if(x+y == y+x){
